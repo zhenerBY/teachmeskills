64b8babe4f4a3d8f73781c11664b585baa37ff31
zhenerBY <zhener@tut.by>
2021-09-15 16:54:32 +0300

    converted the function into a class. First try.

---
+++ range_class.py
@@ -0,0 +1,38 @@
+class rg:
+    """Замена RANGE (tuple)"""
+
+    def __init__(self, start, stop = '@$^*fhYLu', step = '@$^*fhYLu'):
+        if stop == step and stop == '@$^*fhYLu':
+            stop, start, step = start, 0 , 1
+        elif step == '@$^*fhYLu':
+            step = 1
+        self.start = start
+        self.stop = stop
+        self.step = step
+        list1 = []
+        n = start
+        if step > 0:
+            while n < stop:
+                list1.append(n)
+                n += step
+        elif step < 0:
+            while n > stop:
+                list1.append(n)
+                n += step
+        self.tuple = tuple(list1)
+
+    def __getitem__(self, i):
+        return self.tuple[i]
+
+    def __len__(self):
+        return len(self.tuple)
+
+    def __repr__(self):
+        if self.step == 1:
+            return f'range({self.start}, {self.stop})'
+        else:
+            return f'range({self.start}, {self.stop}, {self.step})'
+
+
+
+print(rg(-10, 22, 1))
